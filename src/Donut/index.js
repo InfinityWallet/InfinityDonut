9ee7fe2bce532bb8a333a6d8045974f0839a51ad
Juan <harusurv@hotmail.com>
2021-02-24 20:00:46 +0100

    asd

--- src/Donut/index.js
+++ src/Donut/index.js
@@ -190,7 +190,7 @@ class InfinityDonut extends React.Component {
         for (var i = 0; i < keys.length; i++) {
 
             if (coins_b[keys[i]] != undefined) {
-                this.total_balance += parseFloat(coins_b[keys[i]].amount);
+                this.total_balance += parseFloat(coins_b[keys[i]].amountUSD);
             }
         }
         this.others = [];
@@ -198,19 +198,19 @@ class InfinityDonut extends React.Component {
         if (this.total_balance > 0) {
           for (var i = 0; i < keys.length; i++) {
 
-            var bal = parseFloat(coins_b[keys[i]].amount);
+            var bal = parseFloat(coins_b[keys[i]].amountUSD);
             if (coins_b[keys[i]] == undefined || bal == 0) {
                 continue;
             }
             if (bal / this.total_balance < 0.01) {
                 if (this.balances["others"] == undefined)
                     this.balances["others"] = 0;
-                this.balances["others"] += parseFloat(coins_b[keys[i]].amount);
-                this.others[keys[i]] = parseFloat(coins_b[keys[i]].amount);
+                this.balances["others"] += parseFloat(coins_b[keys[i]].amountUSD);
+                this.others[keys[i]] = parseFloat(coins_b[keys[i]].amountUSD);
 
             }
             else
-                this.balances[keys[i]] = coins_b[keys[i]].amount;
+                this.balances[keys[i]] = coins_b[keys[i]].amountUSD;
             this.num_assets += 1;
           }
         }

0ae64e5ce8474f0fbe77c6f588be5dd544c01dcb
Juan <harusurv@hotmail.com>
2021-02-24 19:57:20 +0100

    fg

--- src/App.js
+++ src/App.js
@@ -3,24 +3,28 @@ const coins = {
   "BTC":{
     "decimals":8,
     "amount":"23924.44",
+    "amountUSD":"234234.234",
     "color":"blue",
     "symbol":"BTC"
   },
   "SHARD":{
     "decimals":8,
     "amount":"113924.44",
+    "amountUSD":"234234.234",
     "color":"green",
     "symbol":"SHARD"
   },
   "LTC":{
     "decimals":8,
     "amount":"392224.44",
+    "amountUSD":"234234.234",
     "color":"pink",
     "symbol":"LTC"
   },
   "ETH":{
     "decimals":8,
     "amount":"392224.44",
+    "amountUSD":"234234.234",
     "color":"red",
     "symbol":"ETH"
   }

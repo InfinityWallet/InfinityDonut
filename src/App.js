8efa08b13c2aa1489b79c0cb24a4c69a192161cb
Juan <harusurv@hotmail.com>
2021-02-27 21:08:19 +0100

    h

--- src/App.js
+++ src/App.js
@@ -2,36 +2,36 @@ import InfinityDonut from './Donut'
 const coins = {
   "BTC":{
     "decimals":8,
-    "amount":"40.44",
-    "amountUSD":"234234.234",
+    "amount":"430.44",
+    "amountUSD":"430.234",
     "color":"blue",
     "symbol":"BTC"
   },
   "SHARD":{
     "decimals":8,
-    "amount":"40.44",
-    "amountUSD":"234234.234",
+    "amount":"404.44",
+    "amountUSD":"430.234",
     "color":"green",
     "symbol":"SHARD"
   },
   "LTC":{
     "decimals":8,
-    "amount":"40.44",
-    "amountUSD":"234234.234",
+    "amount":"430.44",
+    "amountUSD":"480.234",
     "color":"pink",
     "symbol":"LTC"
   },
   "ETH":{
     "decimals":8,
-    "amount":"1.44",
-    "amountUSD":"234234.234",
+    "amount":"13.44",
+    "amountUSD":"13.234",
     "color":"red",
     "symbol":"ETH"
   },
   "ERE":{
     "decimals":8,
-    "amount":"1.44",
-    "amountUSD":"234234.234",
+    "amount":"31.44",
+    "amountUSD":"13.234",
     "color":"pink",
     "symbol":"ERE"
   }
@@ -39,9 +39,18 @@ const coins = {
 export function App(){
 
   return (<InfinityDonut
-          size={500}
           amounts={coins}
+          width={400}
+          height={400}
+
           decimals={2}
+          fontFamily="'Inter var'"
+secondFontColor={"#1890ff"}
+title={"'Inter var'"}
+thirdFontColor={"#2c3753db"}
+backgroundColor={"#000000"}
+fontColor={"#eeeeee"}
+size={1}
           />
     )
 }
